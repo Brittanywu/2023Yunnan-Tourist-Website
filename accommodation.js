5609573768ae8319d4ef29eb3a8d766d0a10d323
Wenyang Zhou <128009825+wenyangviolet@users.noreply.github.com>
2023-04-21 15:52:15 +0800

    Add files via upload

---
+++ accommodation.js
@@ -0,0 +1,149 @@
+function changepage(page) {
+    if(page=='page0') {
+        document.getElementById("hotel_name1").innerText = "Blusher Seaview Hotel";
+        document.getElementById("hotel_text1").innerText = "Located in Dali, 4.6 km from Guanyin Hall, Blusher Seaview Hotel provides accommodations with a shared lounge, free private parking, a terrace and a restaurant.";
+        document.getElementById("price1").innerText = "RMB ¥ 689.00 - 1500.00";
+        document.getElementById("phone1").innerText = " phone: 0508335673 &emsp; address: (Dali-Erhai)";
+        var Image1M = document.getElementById("hotel_Img1");
+        Image1M.src = "images/hotel1.jpg";
+
+        document.getElementById("hotel_name2").innerText = "Lijiang cottage";
+        document.getElementById("hotel_text2").innerText = "Just a short 1-minute walk from Shuhe Ancient Town, Lijiang House features rooms that blend local and modern styles. Guests can use the barbecue facilities, enjoy a soothing massage, go horse riding or have fun time with children at the kids' club.";
+        document.getElementById("price2").innerText = "RMB ¥ 805.00 - 1800.00";
+        document.getElementById("phone2").innerText = "phone: 0508335673 &emsp; address: (Dali-Erhai)";
+        var Image1M = document.getElementById("hotel_Img2");
+        Image1M.src = "images/hotel2.jpg";
+
+        document.getElementById("hotel_name3").innerText = "Kunming Airport Hilton Hotel";
+        document.getElementById("hotel_text3").innerText = "DoubleTree by Hilton Hotel Kunming, 21 km from Kunming East Railway Station, DoubleTree by Hilton Hotel Kunming Airport provides accommodation with a fitness centre, private parking, a garden and a shared lounge.";
+        document.getElementById("price3").innerText = "RMB ¥ 606.00 - 1243.00";
+        document.getElementById("phone3").innerText = "phone: 0800108311 &emsp; address: (Kunming-Guandu District)";
+        var Image1M = document.getElementById("hotel_Img3");
+        Image1M.src = "images/hotel3.jpg";
+
+        document.getElementById("hotel_name4").innerText = "Dali Jia Kodi Cultural Homestay";
+        document.getElementById("hotel_text4").innerText = "Set in Dali, 1.9 km from Huadian Dam, Dali Jia Kedi Cultural B&B features accommodation with a garden, free private parking, a terrace and a restaurant.";
+        document.getElementById("price4").innerText = "RMB ¥ 620.00 - 1686.00";
+        document.getElementById("phone4").innerText = " phone: 0800440800 &emsp; address: (Dali-Xizhou)";
+        var Image1M = document.getElementById("hotel_Img4");
+        Image1M.src = "images/hotel4.jpg";
+
+        document.getElementById("hotel_name5").innerText = "Hotel Indigo Diqing Moonlight City";
+        document.getElementById("hotel_text5").innerHTML = "Featuring a terrace, restaurant, bar and free WiFi, Hotel Indigo Diqing Moonlight City features a terrace, restaurant, bar and free WiFi, a 2-minute walk from Turtle Mountain Park.";
+        document.getElementById("price5").innerHTML = "&RMB ¥ 1200.00 - 3200.00";
+        document.getElementById("phone5").innerHTML = " phone: 0800151252 &emsp; address: (Xianggelila)";
+        var Image1M = document.getElementById("hotel_Img5");
+        Image1M.src = "images/hotel5.jpg";
+    }
+
+    if(page=='page1') {
+        document.getElementById("hotel_name1").innerText = "Blusher Seaview Hotel";
+        document.getElementById("hotel_text1").innerText = "Located in Dali, 4.6 km from Guanyin Hall, Blusher Seaview Hotel provides accommodations with a shared lounge, free private parking, a terrace and a restaurant.";
+        document.getElementById("price1").innerText = "RMB ¥ 689.00 - 1500.00";
+        document.getElementById("phone1").innerText = " phone: 0508335673 &emsp; address: (Dali-Erhai)";
+        var Image1M = document.getElementById("hotel_Img1");
+        Image1M.src = "images/hotel1.jpg";
+
+        document.getElementById("hotel_name2").innerText = "Lijiang cottage";
+        document.getElementById("hotel_text2").innerText = "Just a short 1-minute walk from Shuhe Ancient Town, Lijiang House features rooms that blend local and modern styles. Guests can use the barbecue facilities, enjoy a soothing massage, go horse riding or have fun time with children at the kids' club.";
+        document.getElementById("price2").innerText = "RMB ¥ 805.00 - 1800.00";
+        document.getElementById("phone2").innerText = "phone: 0508335673 &emsp; address: (Dali-Erhai)";
+        var Image1M = document.getElementById("hotel_Img2");
+        Image1M.src = "images/hotel2.jpg";
+
+        document.getElementById("hotel_name3").innerText = " Hilton Kunming Airport Hotel";
+        document.getElementById("hotel_text3").innerText = "DoubleTree by Hilton Hotel Kunming, 21 km from Kunming East Railway Station, DoubleTree by Hilton Hotel Kunming Airport provides accommodation with a fitness centre, private parking, a garden and a shared lounge.";
+        document.getElementById("price3").innerText = "RMB ¥ 606.00 - 1243.00";
+        document.getElementById("phone3").innerText = "phone: 0800108311 &emsp; address: (Kunming-Guandu District)";
+        var Image1M = document.getElementById("hotel_Img3");
+        Image1M.src = "images/hotel3.jpg";
+
+        document.getElementById("hotel_name4").innerText = "Dali Jia Kodi Cultural Homestay";
+        document.getElementById("hotel_text4").innerText = "Set in Dali, 1.9 km from Huadian Dam, Dali Jia Kedi Cultural B&B features accommodation with a garden, free private parking, a terrace and a restaurant.";
+        document.getElementById("price4").innerText = "RMB ¥ 620.00 - 1686.00";
+        document.getElementById("phone4").innerText = " phone: 0800440800 &emsp; address: (Dali-Xizhou)";
+        var Image1M = document.getElementById("hotel_Img4");
+        Image1M.src = "images/hotel4.jpg";
+
+        document.getElementById("hotel_name5").innerText = "Hotel Indigo Diqing Moonlight City";
+        document.getElementById("hotel_text5").innerHTML = "Featuring a terrace, restaurant, bar and free WiFi, Hotel Indigo Diqing Moonlight City features a terrace, restaurant, bar and free WiFi, a 2-minute walk from Turtle Mountain Park.";
+        document.getElementById("price5").innerHTML = "&RMB ¥ 1200.00 - 3200.00";
+        document.getElementById("phone5").innerHTML = " phone: 0800151252 &emsp; address: (Xianggelila)";
+        var Image1M = document.getElementById("hotel_Img5");
+        Image1M.src = "images/hotel5.jpg";
+    }
+
+    if(page=='page2') {
+        document.getElementById("hotel_name1").innerText = "Dongfeng Yun Hotel";
+        document.getElementById("hotel_text1").innerText = "Dongfeng Yunmei Hotel is located in Maitreya. The nearest airport, Munshan Pujahei Airport, is 88 km away. ";
+        document.getElementById("price1").innerText = "RMB ¥ 937.00 - 1432.00";
+        document.getElementById("phone1").innerText = " phone: 2334567901 &emsp; address: (Dali-Erhai)";
+        var Image1M = document.getElementById("hotel_Img1");
+        Image1M.src = "images/hotel6.jpg";
+
+        document.getElementById("hotel_name2").innerText = "Delta Hotel Kunming";
+        document.getElementById("hotel_text2").innerText = "Featuring free WiFi, concierge services and express check-in and check-out, Delta Hotel Kunming is situated in Kunming, 4.4 km from Erji Road Metro Station.";
+        document.getElementById("price2").innerText = "RMB ¥ 805.00 - 1800.00";
+        document.getElementById("phone2").innerText = "phone: 0508335673 &emsp; address: (Dali-Erhai)";
+        var Image1M = document.getElementById("hotel_Img2");
+        Image1M.src = "images/hotel7.jpg";
+
+        document.getElementById("hotel_name3").innerText = "Sheraton Kunming Hotel";
+        document.getElementById("hotel_text3").innerText = "Sheraton Kunming Hotel is conveniently located in Guandu District of Kunming, 4.1 km from Erji Road Station, 5.2 km from Xingyao Road Station and 6 km from Xiaodongcun Station.";
+        document.getElementById("price3").innerText = "RMB ¥ 543.00 - 1269.00";
+        document.getElementById("phone3").innerText = "phone: 0800108311 &emsp; address: (Kunming-Guandu District)";
+        var Image1M = document.getElementById("hotel_Img3");
+        Image1M.src = "images/hotel8.jpg";
+
+        document.getElementById("hotel_name4").innerText = "Xishuangbanna Resort";
+        document.getElementById("hotel_text4").innerText = "This hotel features a spa and wellness centre, a 2,000-square-metre fitness center, an indoor heated pool, a beautiful outdoor pool, a souvenir shop and 6 on-site dining options.";
+        document.getElementById("price4").innerText = "RMB ¥ 368.00 - 1450.00";
+        document.getElementById("phone4").innerText = " phone: 0800440800 &emsp; address: (Dali-Xizhou)";
+        var Image1M = document.getElementById("hotel_Img4");
+        Image1M.src = "images/hotel9.jpg";
+
+        document.getElementById("hotel_name5").innerText = "InterContinental Kunming hotel";
+        document.getElementById("hotel_text5").innerHTML = "Located in Dianchi National Resort on the shores of Dianchi Lake and a 10-minute drive from Yunnan Kunming Nationalities Village, InterContinental Kunming offers free WiFi throughout.";
+        document.getElementById("price5").innerHTML = "&RMB ¥ 1250.00 - 2435.00";
+        document.getElementById("phone5").innerHTML = " phone: 0800151252 &emsp; address: (Dianchi)";
+        var Image1M = document.getElementById("hotel_Img5");
+        Image1M.src = "images/hotel10.jpg";
+    }
+
+    if(page=='page3') {
+        document.getElementById("hotel_name1").innerText = "Dongfeng Yun Hotel";
+        document.getElementById("hotel_text1").innerText = "Dongfeng Yunmei Hotel is located in Maitreya. The nearest airport, Munshan Pujahei Airport, is 88 km away. ";
+        document.getElementById("price1").innerText = "RMB ¥ 937.00 - 1432.00";
+        document.getElementById("phone1").innerText = " phone: 2334567901 &emsp; address: (Dali-Erhai)";
+        var Image1M = document.getElementById("hotel_Img1");
+        Image1M.src = "images/hotel6.jpg";
+
+        document.getElementById("hotel_name2").innerText = "Delta Hotel Kunming";
+        document.getElementById("hotel_text2").innerText = "Featuring free WiFi, concierge services and express check-in and check-out, Delta Hotel Kunming is situated in Kunming, 4.4 km from Erji Road Metro Station.";
+        document.getElementById("price2").innerText = "RMB ¥ 805.00 - 1800.00";
+        document.getElementById("phone2").innerText = "phone: 0508335673 &emsp; address: (Dali-Erhai)";
+        var Image1M = document.getElementById("hotel_Img2");
+        Image1M.src = "images/hotel7.jpg";
+
+        document.getElementById("hotel_name3").innerText = "Sheraton Kunming Hotel";
+        document.getElementById("hotel_text3").innerText = "Sheraton Kunming Hotel is conveniently located in Guandu District of Kunming, 4.1 km from Erji Road Station, 5.2 km from Xingyao Road Station and 6 km from Xiaodongcun Station.";
+        document.getElementById("price3").innerText = "RMB ¥ 543.00 - 1269.00";
+        document.getElementById("phone3").innerText = "phone: 0800108311 &emsp; address: (Kunming-Guandu District)";
+        var Image1M = document.getElementById("hotel_Img3");
+        Image1M.src = "images/hotel8.jpg";
+
+        document.getElementById("hotel_name4").innerText = "Xishuangbanna Resort";
+        document.getElementById("hotel_text4").innerText = "This hotel features a spa and wellness centre, a 2,000-square-metre fitness center, an indoor heated pool, a beautiful outdoor pool, a souvenir shop and 6 on-site dining options.";
+        document.getElementById("price4").innerText = "RMB ¥ 368.00 - 1450.00";
+        document.getElementById("phone4").innerText = " phone: 0800440800 &emsp; address: (Dali-Xizhou)";
+        var Image1M = document.getElementById("hotel_Img4");
+        Image1M.src = "images/hotel9.jpg";
+
+        document.getElementById("hotel_name5").innerText = "InterContinental Kunming hotel";
+        document.getElementById("hotel_text5").innerHTML = "Located in Dianchi National Resort on the shores of Dianchi Lake and a 10-minute drive from Yunnan Kunming Nationalities Village, InterContinental Kunming offers free WiFi throughout.";
+        document.getElementById("price5").innerHTML = "&RMB ¥ 1250.00 - 2435.00";
+        document.getElementById("phone5").innerHTML = " phone: 0800151252 &emsp; address: (Dianchi)";
+        var Image1M = document.getElementById("hotel_Img5");
+        Image1M.src = "images/hotel10.jpg";
+    }
+}
